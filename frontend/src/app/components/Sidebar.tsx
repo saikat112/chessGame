27dabbd955e623864452b8764140fda9b3261d35
saikat112 <saikatmanna112@gmail.com>
2024-08-03 15:53:38 +0530

    dubg

--- frontend/src/app/components/Sidebar.tsx
+++ frontend/src/app/components/Sidebar.tsx
@@ -20,7 +20,7 @@ const SidebarContext = createContext<SidebarContextProps | undefined>(undefined)
 const Sidebar: React.FC<SidebarProps> = ({ children }) => {
   const [expanded, setExpanded] = useState(true);
   const [anchorEl, setAnchorEl] = useState<null | HTMLElement>(null);
-  const { user, setUser } = useUser(); // Destructure both user and setUser
+  const { user, setUser } = useUser();
   const router = useRouter();
 
   const handleMenuClick = (event: MouseEvent<SVGSVGElement>) => {
@@ -35,14 +35,14 @@ const Sidebar: React.FC<SidebarProps> = ({ children }) => {
   };
 
   const handleLogout = () => {
-    localStorage.removeItem('token'); // Remove token from local storage
-    setUser(null); // Clear user context
-    router.push('/login'); // Navigate to login page
+    localStorage.removeItem('token');
+    setUser(null);
+    router.push('/login');
     handleMenuClose();
   };
 
   const handleProfileEdit = () => {
-    router.push('/profile-edit'); // Navigate to profile edit path
+    router.push('/profile-edit');
     handleMenuClose();
   };
 
@@ -50,15 +50,9 @@ const Sidebar: React.FC<SidebarProps> = ({ children }) => {
     <aside className="h-screen">
       <nav className="h-full flex flex-col bg-white border-r shadow-sm">
         <div className="p-4 pb-2 flex justify-between items-center">
-          <img
-            src="https://img.logoipsum.com/243.svg"
-            className={`overflow-hidden transition-all ${expanded ? 'w-32' : 'w-0'}`}
-            alt=""
-          />
+          <img src="https://img.logoipsum.com/243.svg" className={`overflow-hidden transition-all ${expanded ? 'w-32' : 'w-0'}`} alt=""/>
           <button
-            onClick={() => setExpanded((curr) => !curr)}
-            className="p-1.5 rounded-lg bg-gray-50 hover:bg-gray-100"
-          >
+            onClick={() => setExpanded((curr) => !curr)} className="p-1.5 rounded-lg bg-gray-50 hover:bg-gray-100">
             {expanded ? <ChevronFirst /> : <ChevronLast />}
           </button>
         </div>
@@ -70,10 +64,7 @@ const Sidebar: React.FC<SidebarProps> = ({ children }) => {
         <div className="border-t flex p-3">
           <Avatar alt={user?.username} src={user?.avatar || '/profile-icon.png'} sx={{ width: 40, height: 40 }} />
           <div
-            className={`flex justify-between items-center overflow-hidden transition-all ${
-              expanded ? 'w-52 ml-3' : 'w-0'
-            }`}
-          >
+            className={`flex justify-between items-center overflow-hidden transition-all ${expanded ? 'w-52 ml-3' : 'w-0'}`}>
             <div className="leading-4">
               <h4 className="font-semibold">{user?.username}</h4>
               <span className="text-xs text-gray-600">{user?.email}</span>
